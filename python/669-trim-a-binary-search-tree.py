377e149b3c140ec8264c77cdcb5bc22e3921afff
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-04-15 11:25:19 +0530

    ✅ added: 669

---
+++ python/669-trim-a-binary-search-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        # If node.val > high, the trim value should exist on left
+        # If node.val < low, the trim value should exist on right
+        # if equal, then trim left and right respectivly
+        # Untill hits None
+        def trim(node):
+            if not node:
+                return None
+            elif node.val > high:
+                return trim(node.left)
+            elif node.val < low:
+                return trim(node.right)
+            else:
+                node.left = trim(node.left)
+                node.right = trim(node.right)
+                return node
+
+        return trim(root)
