3fab68eb170da18d25f648320f4696fb619e1294
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-04-20 11:30:43 +0530

    ✅ added: 173

---
+++ python/173-binary-search-tree-iterator.py
@@ -0,0 +1,68 @@
+# 🔥 Generator Solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.iter = self._inorder(root)
+        self.nxt = next(self.iter, None)
+
+    def next(self) -> int:
+        res, self.nxt = self.nxt, next(self.iter, None)
+        return res
+
+    def hasNext(self) -> bool:
+        return self.nxt is not None
+        
+    def _inorder(self, node: Optional[TreeNode]) -> Generator[int, None, None]:
+        if node:
+            yield from self._inorder(node.left)
+            yield node.val
+            yield from self._inorder(node.right)
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+
+
+# 🔥 Stack solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.stack = list()
+        self.push(root)
+
+    def next(self) -> int:
+        temp = self.stack.pop()
+        self.push(temp.right)
+        return temp.val
+
+    def hasNext(self) -> bool:
+        return self.stack
+        
+    def push(self, node):
+        while node is not None:
+            self.stack.append(node)
+            node = node.left
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
